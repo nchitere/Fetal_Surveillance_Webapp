ccf646f9614b054f3b0fa9a4cb5a5ffa138c0355
Herbert Netia Chitere <97787733+nchitere@users.noreply.github.com>
2023-08-19 15:42:11 +0300

    Update fetal_monitoring.py

--- fetal_monitoring.py
+++ fetal_monitoring.py
@@ -54,39 +54,6 @@ st.write('Check for class imbalance on the target variable')
 
 """'fetal_health' Tagged as 1 (Normal), 2 (Suspect) and 3 (Pathological)"""
 
-# Calculate counts for each class
-class_counts = fetal['fetal_health'].value_counts()
-
-# Define colors for the three outcomes
-colors = ['blue', 'orange', 'green']
-
-# Calculate the maximum count for scaling the bar heights
-max_count = max(class_counts.values)
-
-# Create a basic bar chart using HTML and CSS
-st.markdown("""
-<style>
-  .bar {
-    display: inline-block;
-    width: 30px;
-    margin: 2px;
-    text-align: center;
-    font-size: 14px;
-    color: white;
-  }
-</style>
-""", unsafe_allow_html=True)
-
-for i, count in enumerate(class_counts):
-    st.markdown(f'<div class="bar" style="height: {count / max_count * 200}px; background-color: {colors[i]};">{count}</div>', unsafe_allow_html=True)
-
-# Add labels and title
-st.markdown('<div style="text-align: center;">Fetal Health</div>', unsafe_allow_html=True)
-st.markdown('<div style="text-align: center;">Count</div>', unsafe_allow_html=True)
-st.markdown('<div style="text-align: center; font-size: 18px;">Distribution of Fetal Health</div>', unsafe_allow_html=True)
-st.write('The figure above shows the distribution of fetal health outcomes where 1=Normal, 2=Suspect, and 3=Pathological')
-
-
 
 
 # import os
@@ -97,6 +64,18 @@ st.write('The figure above shows the distribution of fetal health outcomes where
 # from sklearn.ensemble import RandomForestClassifier
 # from sklearn.metrics import classification_report, confusion_matrix
 # from imblearn.under_sampling import RandomUnderSampler
+
+# Calculate counts for each class
+class_counts = data['fetal_health'].value_counts()
+
+# Create a bar chart using Streamlit's built-in charting function
+st.bar_chart(class_counts)
+
+# Add labels and title
+st.xlabel('Fetal Health')
+st.ylabel('Count')
+st.title('Distribution of Fetal Health')
+st.write('The figure above shows the distribution of fetal health outcomes where 1=Normal, 2=Suspect, and 3=Pathological')
 
 # Create a count plot using Plotly Express
 fig = px.histogram(fetal, x="fetal_health", color ="fetal_health", category_orders={"fetal_health": [1, 2, 3]})
