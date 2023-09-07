120dd8271e5607899d726dc4a7dbcf800789a8a2
Herbert Netia Chitere <97787733+nchitere@users.noreply.github.com>
2023-09-07 06:25:53 +0300

    Update fetal_monitoring.py

--- fetal_monitoring.py
+++ fetal_monitoring.py
@@ -187,7 +187,7 @@ classification_report_imb = classification_report(y_test, y_pred_imb, target_nam
 st.write('Confusion matrix where 0=Normal, 1=Suspect & 2=Pathological')
 st.write("Confusion Matrix (class imbalance uncorrected):\n", confusion_matrix(y_test, y_pred_imb))
 st.write('Classification report')
-st.write("\nClassification Report (class imbalance uncorrected)::\n", classification_report_imb))
+st.write("\nClassification Report (class imbalance uncorrected)::\n", classification_report_imb)
 """The model is performing reasonably well. It has high precision and recall for class 0, \
     meaning it's good at identifying instances of class \
         0(Normal). Class 1 has slightly lower precision and recall, indicating some misclassifications for category suspect. \
